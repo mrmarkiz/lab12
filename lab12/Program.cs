3cc09c322b9bc3c2352d5234dbcf2f36a0b5c0c9
mrmarkiz <maxtomusiak315@gmail.com>
2023-05-09 18:50:39 +0300

    add task1 and task2

--- lab12/Program.cs
+++ lab12/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            int choice;
+            do
+            {
+                Console.Write("Enter task to run: ");
+                int.TryParse(Console.ReadLine(), out choice);
+                switch (choice)
+                {
+                    case 1:
+                        Tasks1_2.Run1();
+                        break;
+                    case 2:
+                        Tasks1_2.Run2();
+                        break;
+                }
+            } while (choice != 0);
         }
     }
 }
---
+++ lab12/Tasks1_2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    internal class Tasks1_2
+    {
+        public static void Run1()
+        {
+            Console.WriteLine($"Max of (3,5,-10) sequence: {Max<int>(3, 5, -10)}");
+            Console.WriteLine($"Max of (-23.1, 6.7, 9.12) sequence: {Max<double>(-23.1, 6.7, 9.12)}");
+        }
+
+        public static void Run2()
+        {
+            Console.WriteLine($"Min of (3,5,-10) sequence: {Min<int>(3, 5, -10)}");
+            Console.WriteLine($"Min of (-23.1, 6.7, 9.12) sequence: {Min<double>(-23.1, 6.7, 9.12)}");
+
+        }
+
+        private static T Min<T>(T first, T second, T third)
+        {
+            dynamic a = (dynamic)first, b = (dynamic)second, c = (dynamic)third;
+            if (a <= b && a <= c)
+                return a;
+            if (b <= a && b <= c)
+                return b;
+            return c;
+        }
+
+        private static T Max<T>(T first, T second, T third)
+        {
+            dynamic a = (dynamic)first, b = (dynamic)second, c = (dynamic)third;
+            if (a >= b && a >= c)
+                return a;
+            if (b >= a && b >= c)
+                return b;
+            return c;
+        }
+    }
+}
